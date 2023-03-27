efd8cc43f25fe8d30b5d3e4f4465d63dcc4d682b
Channel <channel104257@gmail.com>
2023-03-27 11:55:50 +0800

    Update 20230325_1.py

--- Week 5/20230325_1.py
+++ Week 5/20230325_1.py
@@ -1,8 +1,13 @@
-def anonymize_name(name: str) -> str:
+def anonymize_name(name: str):
     if len(name) == 2:
         return name[0] + 'O'
     else:
         return name[0] + 'O' * (len(name) - 2) + name[-1]
-   
-name = input("輸入名字 : ")     
-print(anonymize_name(name))
+
+with open('C:/Users/chann/Desktop/Python程式設計概論與實作/20230327/Names-50.txt', 'r', encoding='utf-8') as f:
+    lines = f.read()
+
+namelist = lines.split('、')
+
+for name in namelist:
+    print(anonymize_name(name),'、', sep='', end='')
