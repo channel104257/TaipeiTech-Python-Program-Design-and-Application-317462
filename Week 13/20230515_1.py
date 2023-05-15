b58db6582ac27f2fb6f4723179030f1f5b131eaa
Channel <channel104257@gmail.com>
2023-05-15 10:37:57 +0800

    Create 20230515_1.py

---
+++ Week 13/20230515_1.py
@@ -0,0 +1,9 @@
+def get_fac(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * get_fac(n - 1)
+
+for i in range(1, 26):
+    result = get_fac(i)
+    print(f"{i}! = {result}")
